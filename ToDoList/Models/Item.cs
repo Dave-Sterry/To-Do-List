bdb6d9b2ad3981a7e5daa9b32e616836efafb0b9
David Sterry <sterry.david@gmail.com>
2020-12-09 10:10:26 -0800

    pass first test
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,14 @@
+namespace ToDoList.Models
+{
+  public class Item
+  {
+      public string Description { get ; }
+
+      public Item(string description)
+      {
+        Description = description;
+      }
+    
+
+  }
+}
